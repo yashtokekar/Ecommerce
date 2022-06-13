c39b908f4bfdfc6a67604c3a729641f6b64285d9
Yash Tokekar <yash.tokekar@gmail.com>
2022-06-13 19:12:51 +0530

    Subcategory CRUD

--- src/App.js
+++ src/App.js
@@ -15,13 +15,15 @@ import { Password } from './pages/user/Password';
 import { Wishlist } from './pages/user/Wishlist';
 import { AdminDashboard } from './pages/admin/AdminDashboard';
 import { CategoryCreate } from './pages/admin/category/CategoryCreate';
+import { CategoryUpdate } from './pages/admin/category/CategoryUpdate';
+import { SubCreate } from './pages/admin/sub/SubCreate';
 
 
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
 
 import { currentUser } from './functions/auth';
-import { CategoryUpdate } from './pages/admin/category/CategoryUpdate';
+
 
 const App = () => {
 const dispatch = useDispatch();
@@ -70,6 +72,7 @@ useEffect(() => {
         <AdminRoute exact path="/admin/dashboard" component={AdminDashboard} />
         <AdminRoute exact path="/admin/category" component={CategoryCreate} />
         <AdminRoute exact path="/admin/category/:slug" component={CategoryUpdate} />
+        <AdminRoute exact path="/admin/sub" component={SubCreate} />
       </Switch>
     </>
   );
---
+++ src/pages/admin/sub/SubCreate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AdminNav } from "../../../components/nav/AdminNav";
+export const SubCreate = () => {
+    return ( 
+        <div className='container-fluid'>
+            <div className="row">
+                <div className="col-md-2">
+                    <AdminNav />
+                </div>
+                <div className="col-md-10">sub create page</div>
+            </div>
+        </div>
+    )
+}
