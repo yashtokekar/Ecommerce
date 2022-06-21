b326973b86e3593a8e0cb62fb054df4209cb74bc
Yash Tokekar <yash.tokekar@gmail.com>
2022-06-21 15:17:35 +0530

    Show all Products

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Card } from 'antd'
+import { Card } from 'antd';
+import image from '../../images/default-image.png';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 export const AdminProductCard = ({ product }) => {
     const { title, description, images } = product;
 
-    return <Card cover={
+    return <Card  cover={
         <img 
-          src={images && images.length ? images[0].url : ""}  
-          style={{ height: "150px", objectFit: "cover" }} />
-    }>
-        <Meta title={title} description={description}></Meta>
+          src={images && images.length ? images[0].url : image}  
+          style={{ height: "150px", objectFit: "cover" }}
+          className="p-1" />
+    }
+    actions={[<EditOutlined className='text-warning' />, <DeleteOutlined className='text-danger' /> ]}
+    >
+        <Meta title={title} description={`${description && description.substring(0,40)}...`}></Meta>
     </Card>
 }
--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,29 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { AdminNav } from "../../components/nav/AdminNav";
-import { getProductsByCount } from "../../functions/product";
-import { LoadingOutlined } from '@ant-design/icons';
-import { AdminProductCard } from "../../components/cards/AdminProductCard";
+
 
 export const AdminDashboard = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() =>{
-        loadAllProducts();
-    }, []);
-
-    const loadAllProducts = () => {
-        setLoading(true);
-        getProductsByCount(100)
-        .then((res) => {
-            setProducts(res.data);
-            setLoading(false);
-        })
-        .catch((err) => {
-            setLoading(false);
-            console.log(err);
-        });
-    };
     
     return (
         <div className="container-fluid">
@@ -32,13 +11,8 @@ export const AdminDashboard = () => {
                     <AdminNav />
                 </div>
                 <div className="col">
-                    {loading ? (<LoadingOutlined className="h1" />) : (<h4 className="col-md-2">All Products</h4>)}
-                    <div className="row">
-                        {products.map((product) => <div className="col-md-4" key={product._id}><AdminProductCard product={product}/></div>)}
-                    </div>
-                </div>
-                
-                
+                    <h4>Admin Dashboard</h4>
+                </div>    
             </div>
         </div>
     );
